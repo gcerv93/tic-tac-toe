1f8b86b7d1b9293dc6e3e15dd26cf72262aaa8c2
gcerv93 <gcerv93@protonmail.com>
2022-01-05 15:43:11 -0600

    add update_board method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -5,16 +5,16 @@ class Board
   attr_reader :board
 
   def initialize
-    @board = starting_board(create_board)
+    @board = starting_board(create_board_array)
   end
 
-  def create_board
+  def create_board_array
     Array.new(3) { Array.new(3) }
   end
 
-  def starting_board(created_board)
+  def starting_board(board_array)
     i = 0
-    created_board.map do |row|
+    board_array.map do |row|
       row.map do |_cell|
         i += 1
       end
@@ -22,7 +22,20 @@ class Board
   end
 
   def display_board
-    @board.each { |row| p row }
+    puts "\n"
+    board.each { |row| p row }
+    puts "\n"
+  end
+
+  def update_board(symbol, location)
+    if location <= 3
+      board[0][location - 1] = symbol
+    elsif location <= 6 && location >= 4
+      board[1][location - 4] = symbol
+    else
+      board[2][location - 7] = symbol
+    end
+    display_board
   end
 end
 
@@ -41,4 +54,8 @@ class Game
   def initialize
     @game_board = Board.new
   end
+
+  def game_loop; end
+
+  def start_game; end
 end
