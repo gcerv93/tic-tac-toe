81383e1c7a4317dbfc315fbc1cfd472d3ea01eda
gcerv93 <gcerv93@protonmail.com>
2022-01-06 14:57:53 -0600

    work with new display

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -5,95 +5,45 @@ class Board
   attr_reader :board, :new_board
 
   def initialize
-    @board = starting_board(create_board_array)
-    @new_board = Array.new(9) { |n| n + 1 }
-  end
-
-  # change
-  def display_board
-    puts "\n"
-    board.each { |row| p row }
-    puts "\n"
+    @board = Array.new(9) { |n| n + 1 }
   end
 
   # rubocop:disable Metrics/AbcSize
-  def display_new_board
+  def display_board
     puts "\n
-    #{new_board[0]} | #{new_board[1]} | #{new_board[2]}
+    #{board[0]} | #{board[1]} | #{board[2]}
     --+---+--
-    #{new_board[3]} | #{new_board[4]} | #{new_board[5]}
+    #{board[3]} | #{board[4]} | #{board[5]}
     --+---+--
-    #{new_board[6]} | #{new_board[7]} | #{new_board[8]}
+    #{board[6]} | #{board[7]} | #{board[8]}
     \n"
   end
   # rubocop:enable Metrics/AbcSize
 
-  # change
   def update_board(symbol, location)
-    if location <= 3
-      board[0][location - 1] = symbol
-    elsif location <= 6 && location >= 4
-      board[1][location - 4] = symbol
-    else
-      board[2][location - 7] = symbol
-    end
+    board[location - 1] = symbol
     display_board
   end
 
-  def update_new_board(symbol, location)
-    new_board[location - 1] = symbol
-    display_new_board
+  def row_win?(symbol)
+    board[0..2].all? { |sym| sym == symbol } ||
+      board[3..5].all? { |sym| sym == symbol } ||
+      board[6..8].all? { |sym| sym == symbol }
   end
 
-  # change
-  def row_win?
-    board.each do |row|
-      return true if row.uniq.length == 1
-    end
-    false
-  end
-
-  def new_row_win?(symbol)
-    new_board[0..2].all? { |sym| sym == symbol } ||
-      new_board[3..5].all? { |sym| sym == symbol } ||
-      new_board[6..8].all? { |sym| sym == symbol }
-  end
-
-  # remove flattening
   def column_win?(symbol)
-    flat = board.flatten
-    flat.values_at(0, 3, 6).all? { |sym| sym == symbol } ||
-      flat.values_at(1, 4, 7).all? { |sym| sym == symbol } ||
-      flat.values_at(2, 5, 8).all? { |sym| sym == symbol }
+    board.values_at(0, 3, 6).all? { |sym| sym == symbol } ||
+      board.values_at(1, 4, 7).all? { |sym| sym == symbol } ||
+      board.values_at(2, 5, 8).all? { |sym| sym == symbol }
   end
 
-  # remove flattening
   def diagonal_win?(symbol)
-    flat = board.flatten
-    return flat[0] == symbol && flat[8] == symbol || flat[2] == symbol && flat[6] == symbol if flat[4] == symbol
+    board.values_at(0, 4, 8).all? { |sym| sym == symbol } ||
+      board.values_at(2, 4, 6).all? { |sym| sym == symbol }
   end
 
-  # remove flattening
   def legal_move?(location)
-    flat = board.flatten
-    return true if flat[location - 1].is_a?(Numeric)
-  end
-
-  private
-
-  # remove
-  def create_board_array
-    Array.new(3) { Array.new(3) }
-  end
- 
-  # remove
-  def starting_board(board_array)
-    i = 0
-    board_array.map do |row|
-      row.map do |_cell|
-        i += 1
-      end
-    end
+    return true if board[location - 1].is_a?(Numeric)
   end
 end
 
@@ -113,7 +63,7 @@ class Player
   end
 
   def player_win?(board)
-    board.row_win? || board.column_win?(symbol) || board.diagonal_win?(symbol)
+    board.row_win?(symbol) || board.column_win?(symbol) || board.diagonal_win?(symbol)
   end
 
   def congratulate_winner
